d716b85aa7cc10a883c1c433c66dbac8cd69f160
JodyFortuin <60154410+JodyFortuin@users.noreply.github.com>
2020-05-12 14:39:27 +0200

    Update calculate-bill.js
    Final commit

--- js/calculate-bill.js
+++ js/calculate-bill.js
@@ -1,29 +1,26 @@
-//get a reference to the calculate button
 const calculateBtn = document.querySelector(".calculateBtn");
-//get a reference to the billTotal element
+
 const phoneBill = document.querySelector(".billTotal");
-//get a reference to the billString
+
 const bill = document.querySelector(".billString");
-//create the function that will be called when the calculate button is pressed
+
 function totalPhoneBill(billString){
-//  * this function should read the string value entered - split it on a comma.
    var callLog = billString.split(', ')
    var billTotal = 0;
-//  * loop over all the entries in the the resulting list
- for(var i=0; i < callLog.length; i++){
+  for(var i=0; i < callLog.length; i++){
+
    var callAction = callLog[i].trim();
-//  * check if it is a call or an sms and add the right amount to the overall total
-//  * once done looping over all the entries - display the total onto the screen in the billTotal element
-       if (callAction === 'call'){
+    if (callAction === 'call'){
        billTotal += 2.75;
     } else if (callAction === 'sms'){
     billTotal += 0.75;
   } 
+    
  } var roundedPhoneBill= billTotal.toFixed(2);
   return billTotal.toFixed(2);
+  
 }
-
-function alertColorChange(roundedPhoneBill){
+   function alertColorChange(roundedPhoneBill){
 if (parseFloat(roundedPhoneBill >= 20 && roundedPhoneBill < 30)) {
     phoneBill.className = "warning";
 }
@@ -31,12 +28,7 @@ else if (parseFloat (roundedPhoneBill >= 30)) {
     phoneBill.className = "danger";
 }
    }
-
+     
 function calculateBtnClicked(){
     var billString = bill.value;
-    const roundedPhoneBill = totalPhoneBill(billString);
-    phoneBill.innerHTML = roundedPhoneBill;
-}
-  
-//link the function to a click event on the calculate button
-calculateBtn.addEventListener('click', calculateBtnClicked);
+    const roundedPhoneBill = to
