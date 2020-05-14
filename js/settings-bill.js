0dc71f83a47be59cc68ed1c6afdf71f32ec4b0f6
JodyFortuin <60154410+JodyFortuin@users.noreply.github.com>
2020-05-14 18:35:27 +0200

    Update settings-bill.js

--- js/settings-bill.js
+++ js/settings-bill.js
@@ -33,10 +33,9 @@ colorCode()
 function setAddBtnClicked(){
    
   var checkedRadioBtnSet = document.querySelector("input[name='billItemTypeWithSettings']:checked");
-  var itemType = checkedRadioBtnSet.value;
   if (totalCostSettings < setCritical){
   if (checkedRadioBtnSet){
- 
+  var itemType = checkedRadioBtnSet.value;
     
   if(itemType === "call"){  
     callCostTotal += setCallCost;
