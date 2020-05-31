33d06e95a5f5171c3c00340cd2cc9e5ff654af3d
JodyFortuin <60154410+JodyFortuin@users.noreply.github.com>
2020-06-01 00:37:16 +0200

    Update radio-bill.js
    separated logic from direct

--- js/radio-bill.js
+++ js/radio-bill.js
@@ -1,39 +1,20 @@
+var radioFact = radioBill();
+
 const billTypeRadio = document.querySelector(".billItemTypeRadio");
 const radioBillAddBtn = document.querySelector(".radioBillAddBtn");
 const callsTotalElemTwo = document.querySelector(".callTotalTwo");
 const smsTotalElemTwo = document.querySelector(".smsTotalTwo");
 const totalCostElemTwo = document.querySelector(".totalTwo");
 
-   var callsT = 0;
-   var smsT = 0;
-
-function radioBillTotal(billItemType){
-   
-    if (billItemType === "call"){
-        callsT += 2.75
-    }
-    else if (billItemType === "sms"){
-        smsT += 0.75
-    }
-    var totalCostTwo= callsT + smsT;
-  return totalCostTwo.toFixed(2);
-  }
-
-
 
   function radioBillAddBtnClicked(){
     var checkedRadioBtn = document.querySelector("input[name='billItemType']:checked");
     var billItemType = checkedRadioBtn.value;
-    const totalCostTwo = radioBillTotal(billItemType);
-    callsTotalElemTwo.innerHTML = callsT.toFixed(2);
-    smsTotalElemTwo.innerHTML = smsT.toFixed(2);
-    totalCostElemTwo.innerHTML = totalCostTwo;
-    
-    if (totalCostTwo >= 50) {
-    totalCostElemTwo.className = "danger";
-    }
-    else if (totalCostTwo >= 30) {
-    totalCostElemTwo.className = "warning";
-    }
+    radioFact.radioBillTotal(billItemType);
+    callsTotalElemTwo.innerHTML = radioFact.getTotalCallCost().toFixed(2);
+    smsTotalElemTwo.innerHTML = radioFact.getTotalSmsCost().toFixed(2);
+    totalCostElemTwo.innerHTML = radioFact.getTotalCost();
+    radioFact.alertColor();
+   
 }
 radioBillAddBtn.addEventListener('click', radioBillAddBtnClicked);
