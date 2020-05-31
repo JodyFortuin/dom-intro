8846a3c1a05f3be3b89e3561c6aa7ef499cf0a1a
JodyFortuin <60154410+JodyFortuin@users.noreply.github.com>
2020-06-01 00:37:54 +0200

    Update text-bill.factory.js

--- tests/text-bill.factory.js
+++ tests/text-bill.factory.js
@@ -1,10 +1,26 @@
 function textBill(){
-    var theCallCost = 0;
-    var theSmsCost = 0;
+    var theCallCost = 2.75;
+    var theSmsCost = 0.75;
     var theWarnLevel = 30;
     var theCritLevel = 50;
     var callCostTotal = 0;
     var smsCostTotal = 0;
+
+    function textBillTotal(billString){
+        var billTypeEntered = billString.split(',');
+        
+       for(var i=0; i < billTypeEntered.length; i++){
+         var callAction = billTypeEntered[i].trim();
+         if (callAction === "call"){
+             textFact.addCall();
+         }
+         else if (callAction === "sms"){
+             textFact.addSms();
+         }
+         var totalCost= textFact.getTotalCallCost() + textFact.getTotalSmsCost();
+       return totalCost.toFixed(2);
+       }
+     }
 
     function setCallCost(callCost){
         theCallCost = callCost;
@@ -53,12 +69,12 @@ function textBill(){
     }
 
     function alertColor(){
-        if (getTotalCost() >= getCritLevel()) {
-            return "critical";
-            } 
-        if (getTotalCost() >= getWarnLevel()) {
-            return "warning";
-            }
+        if (getTotalCost() >= 30 && getTotalCost() < 50) {
+            totalCostElem.className = "warning";
+        }
+        else if (getTotalCost() >= 50) {
+            totalCostElem.className = "danger";
+        }
        }
     return {
         setCallCost,
@@ -74,5 +90,7 @@ function textBill(){
         getTotalSmsCost,
         addSms,
         alertColor,
+        textBillTotal,
     }
 }
+
