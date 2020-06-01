3565aff460f6f28d38957b87d2f65e7cfb7c7541
JodyFortuin <60154410+JodyFortuin@users.noreply.github.com>
2020-06-01 18:50:05 +0200

    Update settings-bill.js
    Stopped adding when critical is reached + logic issues

--- js/settings-bill.js
+++ js/settings-bill.js
@@ -12,15 +12,28 @@ const warningSetElem = document.querySelector(".warningLevelSetting");
 const criticalSetElem = document.querySelector(".criticalLevelSetting");
 const updateSetElem = document.querySelector(".updateSettings");
 const setAddBtnElem = document.querySelector(".addButton");
+
+var setCallCost = 0;
+var setSmsCost = 0;
+var setWarning = 0;
+var setCritical = 0;
+
+var callCostTotal = 0;
+var smsCostTotal = 0;
+var totalCostSettings = 0;
   
 function update(){
   
- setCallCost = Number(callCostSetElem.value)
- setSmsCost = Number(smsCostSetElem.value)
- setWarning = Number(warningSetElem.value)
- setCritical = Number(criticalSetElem.value)
-colorCode()
+ 
+   
+ settingsFact.setCallCost(Number(callCostSetElem.value));
+ settingsFact.setSmsCost(Number(smsCostSetElem.value));
+ settingsFact.setWarnLevel(Number(warningSetElem.value));
+ settingsFact.setCritLevel(Number(criticalSetElem.value));
+ 
 }
+
+
 
 
 
@@ -29,14 +42,13 @@ function setAddBtnClicked(){
   var checkedRadioBtnSet = document.querySelector("input[name='billItemTypeWithSettings']:checked");
   var itemType = checkedRadioBtnSet.value;
   settingsFact.settingsPhoneBill(itemType);
-    callTotalSetElem.innerHTML = settingsFact.getTotalCallCost.toFixed(2);
-    smsTotalSetElem.innerHTML = settingsFact.getTotalSmsCost.toFixed(2);
-    const totalCostSettings = settingsPhoneBill(itemType);
-    alertColor()
+  callTotalSetElem.innerHTML = settingsFact.getTotalCallCost().toFixed(2);
+  smsTotalSetElem.innerHTML = settingsFact.getTotalSmsCost().toFixed(2);
     
-    totalSetElem.innerHTML = settingsFact.getTotalCost().toFixed(2);
-}
-
+  totalSetElem.innerHTML = settingsFact.getTotalCost().toFixed(2);
+  settingsFact.alertColor();
+       
+  }
 
 updateSetElem.addEventListener("click", update);
 setAddBtnElem.addEventListener("click", setAddBtnClicked);
